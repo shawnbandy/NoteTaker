164705789fbe15fa0be0af20cb6681e85c5b14fa
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-09-13 13:08:03 -0400

    checked off most of the readme criteria, started on the bonus

--- Develop/server.js
+++ Develop/server.js
@@ -7,10 +7,7 @@ const fs = require('fs');
 //*this will make a random charset with numbers and put it to string
 //*this is courtesy of Assignment 19
 //*I may change it to be something else
-const randomID = () =>{
-    Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-}
-
+const randomID = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 
 const app = express();
 
@@ -23,7 +20,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname), '/public/index.html')
 })
 
@@ -42,28 +39,28 @@ app.post('/api/notes', (req, res) => {
         const incomingNote = {
             title,
             text,
-            id: randomID
+            id: randomID()
         }
 
         db.push(incomingNote)
 
         const response = {
             status: 'Success!',
-            body: incomingNote
+            body: `${incomingNote.title}, ${incomingNote.text}, ${incomingNote.id}`
         };
 
         const newDB = JSON.stringify(db);
 
-        fs.writeFile(`./db/reviews.json`, newDB, (err) =>{
-            if (err){
+        //*writing this to a separate file for now
+        fs.writeFile(`./db/db.json`, newDB, (err) => {
+            if (err) {
                 console.log(err)
-            }else{
+            } else {
                 console.log('Database has been updated with your note')
             }
         })
 
-
-        res.status(201).json(`${response.status}, here is the note you just logged: ${response.body}`)
+        res.status(201).json(`${response.status} here is the note you just logged: ${response.body}`)
 
     } else {
         res.status(500).json(`There was an error, please try again`)
@@ -73,9 +70,32 @@ app.post('/api/notes', (req, res) => {
 })
 
 //!this will be app.delete if I can get it working
-// app.delete('/api/notes/:id', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
+    console.info(`${req.method} requested`);
 
-// })
+    const deleteItem = req.params.id;
+
+    if(db.some(note => note.id == deleteItem)){
+
+        for (let i = 0; i < db.length; i++) {
+
+            console.info(db[i].id)
+
+            if (deleteItem == db[i].id) {
+                console.info('Match found')
+                db.filter(note => note.id == deleteItem )
+                return res.json(db)
+            } 
+
+        }
+
+    }else {
+        console.info('No ID found')
+    }
+
+    res.json('No ID Found')
+
+})
 
 
 app.listen(PORT, () => {
--- README.md
+++ README.md
@@ -4,27 +4,27 @@
     1. Should open to the landing page of the index.html ✅
     2. Clicking on the 'notes' page link redirects you to the notes.html ✅
     3. Typing a note title and note text shows a save icon ✅
-    4. When a note is saved, it appears on the left hand column
-    5. Clicking on a note in the left hand column puts it in the right hand column 
+    4. When a note is saved, it appears on the left hand column ✅
+    5. Clicking on a note in the left hand column puts it in the right hand column ✅
     6. Clicking the 'write icon' presents empty note fields ✅
     7. Application must use Express.js ✅
-    8. Notes must have a unique ID in the JSON file
+    8. Notes must have a unique ID in the JSON file ✅
     9. Application must be deployed to Heroku
 
 ### Application specifics:
     1. server.js should include db.json file ✅
     2. server.js should include fs module ✅
-    3. GET /notes should return notes.html (can check w/ insomnia)
-    4. GET * should return index.html (can check w/ insomnia)
-    5. GET api/notes should read the db.json file and return all saved notes (can check w/ insomnia)
+    3. GET /notes should return notes.html (can check w/ insomnia) ✅
+    4. GET * should return index.html (can check w/ insomnia) ✅
+    5. GET api/notes should read the db.json file and return all saved notes (can check w/ insomnia) ✅
     6. POST api/notes should:
-        a. receive a new note on the request body
-        b. add it to the db.json file
-        c. return the new note to the client 
-        d. each note should have a unique ID
+        a. receive a new note on the request body ✅
+        b. add it to the db.json file ✅
+        c. return the new note to the client ✅
+        d. each note should have a unique ID ✅
 
 ### Bonus
-    1. Delete /api/notes/:id should receive a query parameter of the note id 
+    1. Delete /api/notes/:id should receive a query parameter of the note id ✅
     2. Delete should remove the note via its id property from the db.json file
 
 
@@ -33,4 +33,5 @@ Application Description:
 
 
 Application URL: 
+
 Application GitHub: 
