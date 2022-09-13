74d958fd23f2c0d1185336c2ae654d8fb2781a0f
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-09-12 21:33:54 -0400

    added more to the server.js and POST. Have not tested yet

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,83 @@
 const express = require('express');
 const path = require('path');
-const db = require('./db/db.json');
+const db = require('./db/db.json'); //*db = database
 const PORT = 3001;
+const fs = require('fs');
+
+//*this will make a random charset with numbers and put it to string
+//*this is courtesy of Assignment 19
+//*I may change it to be something else
+const randomID = () =>{
+    Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+}
 
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) =>{
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
-app.use(express.json());
-app.use(express.urlencoded({extended : true }));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname), '/public/index.html')
+})
 
-app.listen(PORT, () =>{
+app.get('/api/notes', (req, res) => {
+    console.log('request for note api received')
+    res.status(200).json(db)
+})
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} requested`)
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+
+        const incomingNote = {
+            title,
+            text,
+            id: randomID
+        }
+
+        db.push(incomingNote)
+
+        const response = {
+            status: 'Success!',
+            body: incomingNote
+        };
+
+        const newDB = JSON.stringify(db);
+
+        fs.writeFile(`./db/reviews.json`, newDB, (err) =>{
+            if (err){
+                console.log(err)
+            }else{
+                console.log('Database has been updated with your note')
+            }
+        })
+
+
+        res.status(201).json(`${response.status}, here is the note you just logged: ${response.body}`)
+
+    } else {
+        res.status(500).json(`There was an error, please try again`)
+    }
+
+
+})
+
+//!this will be app.delete if I can get it working
+// app.delete('/api/notes/:id', (req, res) => {
+
+// })
+
+
+app.listen(PORT, () => {
     console.log(`Port is live http://localhost:3001`)
 })
--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Note-taker assignment
 
 ### Acceptance Criteria: 
-    1. Should open to the landing page of the index.html
-    2. Clicking on the 'notes' page link redirects you to the notes.html
-    3. Typing a note title and note text shows a save icon 
+    1. Should open to the landing page of the index.html ✅
+    2. Clicking on the 'notes' page link redirects you to the notes.html ✅
+    3. Typing a note title and note text shows a save icon ✅
     4. When a note is saved, it appears on the left hand column
     5. Clicking on a note in the left hand column puts it in the right hand column 
-    6. Clicking the 'write icon' presents empty note fields 
-    7. Application must use Express.js
+    6. Clicking the 'write icon' presents empty note fields ✅
+    7. Application must use Express.js ✅
     8. Notes must have a unique ID in the JSON file
     9. Application must be deployed to Heroku
 
 ### Application specifics:
-    1. server.js should include db.json file 
-    2. server.js should include fs module
+    1. server.js should include db.json file ✅
+    2. server.js should include fs module ✅
     3. GET /notes should return notes.html (can check w/ insomnia)
     4. GET * should return index.html (can check w/ insomnia)
     5. GET api/notes should read the db.json file and return all saved notes (can check w/ insomnia)
