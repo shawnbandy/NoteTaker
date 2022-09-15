92e5cb42f814d3b1611fe28ad4996fa2ae1dfc52
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-09-15 08:54:53 -0400

    m

--- Develop/server.js
+++ Develop/server.js
@@ -17,13 +17,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname), '/public/index.html')
+})
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname), '/public/index.html')
-})
+
 
 app.get('/api/notes', (req, res) => {
     console.log('request for note api received')
