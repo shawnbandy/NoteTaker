398c7f9d4a6576627048583404422000cca84ed7
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-09-15 08:05:54 -0400

    m

--- Develop/server.js
+++ Develop/server.js
@@ -20,7 +20,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
-app.get('*', (req, res) => {
+app.get('/.*/', (req, res) => {
     res.sendFile(path.join(__dirname), '/public/index.html')
 })
 
