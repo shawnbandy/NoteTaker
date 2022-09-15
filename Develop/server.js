5fe8b041624828b1c4389110746da5486bf67517
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-09-15 09:24:40 -0400

    m

--- Develop/server.js
+++ Develop/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static('/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
