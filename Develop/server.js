1d29d125e1a52f56f6db2ff069d493054d32a1f3
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-09-15 08:14:11 -0400

    m

--- Develop/server.js
+++ Develop/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
